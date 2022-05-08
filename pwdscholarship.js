c505c8f66667953f54212ff2d9ad060054d65385
Alay kabir <67549818+alaykabir@users.noreply.github.com>
2022-05-08 11:27:19 +0530

    Add files via upload

---
+++ pwdscholarship.js
@@ -0,0 +1,278 @@
+var scholarship=[
+    {
+        name: "Pre-matric Scholarship for Students with Disabilities",
+        familyincome: 200000,
+        reward: "Stipend, book and additional allowances",
+        percentagedis: 40,
+        standard:"10TH",
+        website: "www.scholarships.gov.in "
+    },
+    {
+        name: "AICTE - Saksham Scholarship Scheme",
+        familyincome: 800000,
+        reward: "Total 1000 scholarship are given @ Rs. 30000/- as tuition fee reimbursement and Rs. 20000/- as incidentals each year",
+        percentagedis: 40,
+        // standard:10,
+        standard:"GRADUATION",
+        website:"https://www.aicte-india.org/schemes/students-development-schemes/Saksham/General-Instructions?msclkid=bfb5e4d7ce7211eca567649f101ddcfb"
+    },
+    {
+        name: "Loan for Professional/Educational/Training Courses to Disabled Persons",
+        familyincome: 1000000000,
+        reward: "Maximum INR 10.00 lacs for study in India and maximum INR 20.00 lacs for abroad studies. Rate of interest – 4% per annum",
+        percentagedis: 40,
+        // standard:10,
+        standard:"GRADUATION",
+        website:"http://www.nhfdc.nic.in/site/Education_scheme.pdf?msclkid=4767589dce7311ec88867e73493bdb1f"
+    },
+    // {
+    //     name: "National Talent Search Scheme",
+    //     familyincome: 1000000000,
+    //     reward: "RS 1250 per month",
+    //     percentage: 0,
+    //     // standard:10,
+    //     standard:"10TH",
+    //     website:"http://ntse.ncert.gov.in/login.aspx?ReturnUrl=%2f"
+    // },
+    // {
+    //     name: "Aakash Anthe",
+    //     familyincome: 1000000000,
+    //     reward: "Admission into differnet course upto 1 lakh",
+    //     percentage: 0,
+    //     // standard:10,
+    //     standard:"10TH",
+    //     website:"https://www.aakash.ac.in/anthe"
+    // },
+    // {
+    //     name: "Ujjwal Bhavishya Scholarship",
+    //     familyincome: 200000,
+    //     reward:10000,
+    //     percentage: 85,
+    //     // standard:10,
+    //     standard:"10TH",
+    //     website:"https://www.registrationstatus.in/ujjwal-bhavishya-scholarship/"
+    // },
+    // {
+    //     name: "Siksha Abhiyan Scholarship",
+    //     familyincome: 1000000000,
+    //     reward:"5-100% fee waiver + different cash prize for top rankholder",
+    //     percentage: 75,
+    //     // standard:10,
+    //     standard:"10TH",
+        
+    //     website:"https://www.registrationstatus.in/ujjwal-bhavishya-scholarship/"
+    // },
+    // {
+    //     name: "Prime Minister Scholarship",
+    //     familyincome: 1000000000,
+    //     reward:"Rs 2000/month for boys and Rs 2250/month for girls",
+    //     percentage: 60,
+    //     // standard: 12,
+    //     standard:"12TH",
+        
+    //     website:"www.ksb.gov.in"
+    // },
+    // {
+    //     name: "Hdfc Bank Scholarship",
+    //     familyincome: 100000,
+    //     reward:"RS 10,000 for school students and RS 35000 for college student",
+    //     percentage: 60,
+    //     // standard: 12,
+    //     standard:"12TH",
+        
+    //     website:"www.ksb.gov.in"
+    // },
+    // {
+    //     name: "Colgate Scholarship",
+    //     familyincome: 500000,
+    //     reward: 35000,
+    //     percentage: 75,
+    //     // standard: 12,
+    //     standard:"12TH",
+        
+    //     website: "www.Colgate.co.in"
+    // },
+    // {
+    //     name: "Aakash Anthe",
+    //     familyincome: 1000000000,
+    //     reward: "Admission into differnet course upto 1 lakh",
+    //     percentage: 0,
+    //     // standard: 12,
+    //     standard:"12TH",
+       
+    //     website:"https://www.aakash.ac.in/anthe"
+    // },
+    // {
+    //     name: "Ujjwal Bhavishya Scholarship",
+    //     familyincome: 200000,
+    //     reward:10000,
+    //     percentage: 85,
+    //     // standard: 12,
+    //     standard:"12TH",
+        
+    //     website:"https://www.registrationstatus.in/ujjwal-bhavishya-scholarship/"
+    // },
+
+
+
+
+];
+
+
+
+//  const scholarship1=new scholarship("Colgate Scholarship",100000,35000,75,10,"www.Colgate.co.in");
+//  const scholarship2=new scholarship("Indian Oil Scolarship(IOCL)",100000,"RS 2000 per month",65,10,"https://www.iocl.com/");
+//  const scholarship3=new scholarship("Jio Scholarship",1000000000,35000,85,10,"https://www.jio.com/");
+//  const scholarship4=new scholarship("National Talent Search Scheme",1000000000,"RS 1250 per month",0,10,"http://ntse.ncert.gov.in/login.aspx?ReturnUrl=%2f");
+//  const scholarship5=new scholarship("Aakash Anthe",1000000000,"Admission into differnet course upto 1 lakh",0,10,"https://www.aakash.ac.in/anthe");
+//  const scholarship6=new scholarship("Ujjwal Bhavishya Scholarship",200000,10000,85,10,"https://www.registrationstatus.in/ujjwal-bhavishya-scholarship/");
+//  const scholarship7=new scholarship("Siksha Abhiyan Scholarship",1000000000,"5-100% fee waiver + different cash prize for top rankholder",75,10,"https://www.registrationstatus.in/ujjwal-bhavishya-scholarship/");
+//  const scholarship8=new scholarship("Prime Minister Scholarship",100000000," Rs 2000/month for boys and Rs 2250/month for girls",60,12,"www.ksb.gov.in");
+//  const scholarship9=new scholarship("Hdfc Bank Scholarship",100000,"RS 10,000 for school students and RS 35000 for college student",60,12,"www.hdfcbank.com");
+//  const scholarship10=new scholarship("Colgate Scholarship",500000,35000,75,12,"www.Colgate.co.in");
+//  const scholarship11=new scholarship("Aakash Anthe",1000000000,"Admission into differnet course upto 1 lakh",0,12,"https://www.aakash.ac.in/anthe");
+//  const scholarship12=new scholarship("Ujjwal Bhavishya Scholarship",200000,10000,85,12,"https://www.registrationstatus.in/ujjwal-bhavishya-scholarship/");
+// var standardinput=prompt("enter your highest qualification");
+// var familyincomeinput=prompt("enter your family income");
+// var percentageinclass=prompt("enter the marks");
+
+
+// var standardinput=prompt("enter your highest qualification");
+// var familyincomeinput=prompt("enter your family income");
+// var percentageinclass=prompt("enter the marks");
+
+
+// if(familyincomeinput==undefined)
+// {
+//     familyincomeinput=0;
+// }
+// if(percentageinclass==undefined)
+// {
+//     percentageinclass=0;
+// }
+// for(var key in scholarship)
+// {
+    // if(scholarship[i].familyincome>familyincomeinput&&scholarship[i].percentage<percentageinclass)
+    // {
+        
+    //      if(scholarship[i].standard==undefined)
+    //      {
+    //          console.log(scholarship[i]);
+    //      }
+    //      else if(scholarship[i].standardinput==10)
+    //      {
+    //         console.log(scholarship[i]);
+    //      }
+    //      else if(scholarship[i].standardinput==12)
+    //      {
+    //         console.log(scholarship[i]);
+    //      }
+
+    // }
+    // console.log(scholarship.key);
+// }
+// var familyincomeinput=prompt("enter family income");
+// var standardinput=prompt("enter standard");
+// var percentageinclass=prompt("enter the percentage");
+
+
+var standardinput=-1;
+var disabilitpercentageinput=50;
+var familyincomeinput=-1;
+
+const scholarshipgovt={
+    name: "National Scholarship Portal",
+    details:"You have to upload all documents this government potal will apply for all govt. scholarships",
+    eligibilty:"for pre and post matric both are eligible for this",
+    website :"https://scholarships.gov.in/fresh/newstdRegfrmInstruction"
+
+}
+// if(familyincomeinput===undefined)
+// {
+//     familyincomeinput=1;
+// }
+// if(percentageinclass===undefined)
+// {
+//     percentageinclass=2;
+// }
+// // if(standardinput==undefined)
+// {
+    
+// // }
+// console.log(familyincomeinput)
+// console.log(percentageinclass);
+if(standardinput===-1&&familyincomeinput===-1&&disabilitpercentageinput===-1)
+{
+    for(var i=0;i<scholarship.length;++i)
+    {
+        console.log(scholarship[i]);
+    }
+
+}
+else if(standardinput===-1&&familyincomeinput===-1&&disabilitpercentageinput!=-1)
+{
+    for(var i=0;i<scholarship.length;++i)
+    {
+        if(scholarship[i].percentagedis<disabilitpercentageinput)
+        console.log(scholarship[i]);
+    }
+}
+else if(standardinput!=-1&&familyincomeinput===-1&&disabilitpercentageinput===-1)
+{
+    for(var i=0;i<scholarship.length;++i)
+    {
+        if(scholarship[i].standard===standardinput)
+        console.log(scholarship[i]);
+    }
+}
+else if(standardinput===-1&&familyincomeinput!=-1&&disabilitpercentageinput===-1)
+{
+    for(var i=0;i<scholarship.length;++i)
+    {
+        if(scholarship[i].familyincome>familyincomeinput)
+        console.log(scholarship[i]);
+    }
+}
+else if(standardinput===-1&&disabilitpercentageinput!=-1&&familyincomeinput!=-1)
+{
+    for(var i=0;i<scholarship.length;++i)
+    {
+        if(scholarship[i].familyincome>familyincomeinput&&scholarship[i].percentagedis<disabilitpercentageinput)
+        console.log(scholarship[i]);
+    }
+}
+else if(standardinput!=-1&&disabilitpercentageinput===-1&&familyincomeinput!=-1)
+{
+    for(var i=0;i<scholarship.length;++i)
+    {
+        if(scholarship[i].familyincome>familyincomeinput&&scholarship[i].standard===standardinput)
+        console.log(scholarship[i]);
+    }
+}
+else if(standardinput!=-1&&disabilitpercentageinput!=-1&&familyincomeinput===-1)
+{
+    for(var i=0;i<scholarship.length;++i)
+    {
+        if(scholarship[i].percentagedis<disabilitpercentageinput&&scholarship[i].standard===standardinput)
+        console.log(scholarship[i]);
+    }
+}
+
+else{
+    for(var i=0;i<scholarship.length;++i)
+    {
+        
+        console.log(scholarship[i]);
+    }
+}
+
+// console.log(scholarshipgovt)
+    // else if(standardinput===scholarship[i].standard&&disabilitpercentageinput===undefined&&familyincomeinput===undefined)
+    // {
+    //     console.log(scholarship[i]);
+    //     continue;
+    // }
+
+       
+    
+
